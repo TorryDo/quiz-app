1ad147a8b7e6524788ba4ea77580dab1ba3c80e1
DESKTOP-DJRPVMC\trido <torrydo.mobile@gmail.com>
2022-11-21 17:02:56 +0700

    recreate fake_repository

--- lib/src/features/quiz/data/repository/fake_topic_repository_impl.dart
+++ lib/src/features/quiz/data/repository/fake_topic_repository_impl.dart
@@ -1,3 +1,5 @@
+import 'package:quiz_app/utils/lang/list_ext.dart';
+
 import '../../../../../utils/logger.dart';
 import '../../domain/models/question.dart';
 import '../../domain/models/question_answer.dart';
@@ -7,69 +9,81 @@ import '../../domain/models/volume.dart';
 import '../../domain/repository/quiz_repository.dart';
 
 class FakeQuizRepositoryImpl extends QuizRepository with Logger {
-  final _fakeTopic = Topic(
-      id: 0, title: "super awesome title", description: "awesome description");
+  final _fakeTopics = [
+    Topic(id: 1, title: "Topic 1", description: "description of topic1"),
+    Topic(id: 2, title: "Topic 2", description: "description of topic2"),
+    Topic(id: 3, title: "Topic 3", description: "description of topic3"),
+  ];
 
-  final _fakeVolume = Volume(id: 0, topicId: 0, title: 'not awesome volume');
+  final _fakeVolumes = [
+    Volume(id: 1, topicId: 1, title: 'volume 1'),
+    Volume(id: 2, topicId: 1, title: 'volume 2'),
+    Volume(id: 3, topicId: 1, title: 'volume 3'),
+    Volume(id: 4, topicId: 1, title: 'volume 4'),
+    Volume(id: 5, topicId: 1, title: 'volume 5'),
+    // -----------------------------------------
+    Volume(id: 6, topicId: 2, title: 'volume 6'),
+    Volume(id: 7, topicId: 2, title: 'volume 7'),
+    Volume(id: 8, topicId: 2, title: 'volume 8'),
+    Volume(id: 9, topicId: 2, title: 'volume 9'),
+    // -----------------------------------------
+    Volume(id: 10, topicId: 3, title: 'volume 10'),
+    Volume(id: 11, topicId: 3, title: 'volume 11'),
+    Volume(id: 12, topicId: 3, title: 'volume 12'),
+  ];
 
-  final _fakeQuestion = Question(
-      id: 0,
-      volumeId: 0,
-      questionDescription: DescriptionOnly("who developed this app?"),
-      questionAnswer: AnswerFromOptions(
-          options: ["torrydo", "abc", "xyz", "hahaha"], position: 0));
+  final List<Question> _fakeQuestions = [];
+
+  FakeQuizRepositoryImpl() {
+    int temp = 1;
+    for (int i = 1; i <= 36; i++) {
+      _fakeQuestions.add(
+        Question(
+          id: i,
+          volumeId: temp,
+          questionDescription: DescriptionOnly(
+            'who developed this app? id$i',
+          ),
+          questionAnswer: AnswerFromOptions(
+            options: ["torrydo", "abc", "xyz", "hahaha"],
+            position: 0,
+          ),
+        ),
+      );
+      if (i % 3 == 0) {
+        temp++;
+      }
+    }
+  }
 
   @override
   Stream<List<Topic>> collectTopics() async* {
     e("return fake data");
-
-    List<Topic> tl = [];
-
-    for (var i = 0; i < 15; i++) {
-      var temp = (_fakeTopic.copyWith(id: i, title: 'topic title $i'));
-      tl.add(temp);
-    }
-
-    yield tl;
+    yield _fakeTopics;
   }
 
   @override
   Stream<List<Question>> collectQuestions() async* {
     e("return fake data");
-
-    List<Question> ql = [];
-
-    for (var i = 0; i < 5; i++) {
-      var temp = (_fakeQuestion.copyWith(id: i));
-      ql.add(temp);
-    }
-
-    yield ql;
+    yield _fakeQuestions;
   }
 
   @override
   Stream<List<Question>> collectQuestionsByVolume(Volume volume) async* {
     e("return fake data");
-    yield [_fakeQuestion];
+    yield _fakeQuestions.filter((element) => element.volumeId == volume.id);
   }
 
   @override
   Stream<List<Volume>> collectVolumes() async* {
     e("return fake data");
-    yield [_fakeVolume];
+    yield _fakeVolumes;
   }
 
   @override
   Stream<List<Volume>> collectVolumesByTopic(Topic topic) async* {
     e("return fake data");
-    List<Volume> vl = [];
-
-    for (var i = 0; i < 3; i++) {
-      var temp = (_fakeVolume.copyWith(
-          id: i, topicId: topic.id, title: "volume number $i"));
-      vl.add(temp);
-    }
-    yield vl;
+    yield _fakeVolumes.filter((it) => it.topicId == topic.id);
   }
 
   @override
@@ -77,36 +91,13 @@ class FakeQuizRepositoryImpl extends QuizRepository with Logger {
     e("return fake data");
     List<Question> ql = [];
 
-
-
-    for (var i = 0; i < 3; i++) {
-      var tempAnswer = AnswerFromOptions(
-          options: ['torydo$i', 'idk', 'dkm', 'kut'], position: 0);
-
-      var temp = (_fakeQuestion.copyWith(
-        id: i,
-        questionDescription: DescriptionOnly('who developed this app ${i}?'),
-        questionAnswer: tempAnswer,
-        volumeId: volume.id
-      ));
-      ql.add(temp);
-    }
-    return ql;
+    return _fakeQuestions.filter((it) => it.volumeId == volume.id);
   }
 
   @override
   List<Volume> getVolumesByTopic(Topic topic) {
-    List<Volume> vl = [];
+    e("return fake data");
 
-    for (var i = 0; i < 10; i++) {
-      final temp = (_fakeVolume.copyWith(
-        id: i,
-        topicId: topic.id,
-        title: 'not awesome volume $i',
-      ));
-      vl.add(temp);
-    }
-
-    return vl;
+    return _fakeVolumes.filter((it) => it.topicId == topic.id);
   }
 }
--- lib/utils/lang/list_ext.dart
+++ lib/utils/lang/list_ext.dart
@@ -1,3 +1,4 @@
+
 extension ListX<T> on List<T> {
   /// clear();
   ///
@@ -7,18 +8,31 @@ extension ListX<T> on List<T> {
     addAll(iterable);
   }
 
-  List<E> mapIndexed<E>(E Function(T, int) adapter) {
+  List<E> mapIndexed<E>(E Function(T it, int index) adapter) {
     List<E> rs = [];
     for (var i = 0; i < length; i++) {
       rs.add(adapter(this[i], i));
     }
     return rs;
   }
-  List<E> mapTo<E>(E Function(T) adapter) {
+
+  List<E> mapTo<E>(E Function(T it) adapter) {
     List<E> rs = [];
     for (var i = 0; i < length; i++) {
       rs.add(adapter(this[i]));
     }
+    return rs;
+  }
+
+  List<T> filter(bool Function(T it) adapter) {
+    List<T> rs = [];
+
+    for (final item in this) {
+      if (adapter(item)) {
+        rs.add(item);
+      }
+    }
+
     return rs;
   }
 }
